9d65475e90c712b9f6503bd9b198c09550936f56
JKeun <jkeun1224@gmail.com>
2016-10-29 12:46:37 +0900

    add projectsRouter:
    add projectsRouter var in server.js
    add li projects in header.pug
    add projects.js views/projects/list, sample.pug to route & rendering

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var app = express();
 
 var indexRouter = require('./routes/index')(app);
 var aboutRouter = require('./routes/about')(app);
+var projectsRouter = require('./routes/projects')(app);
 
 var pug = require('pug');
 var path = require('path');
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname + "/views"));
+app.set("projects", path.join(__dirname + "/projects"));
 
 app.set("port", process.env.PORT || 3030);
 
