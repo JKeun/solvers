6e5f0497e1e359008cb96b9ceacb80dcc8b8a99d
JKeun <jkeun1224@gmail.com>
2016-12-10 17:22:38 +0900

    fixed navbar for scroll

---
+++ public/js/site.js
@@ -0,0 +1,7 @@
+$(window).scroll(function() {
+	if($(this).scrollTop() > 50) {
+		$('.dc-navbar-custom').addClass('dc-navbar-fixed');
+	} else {
+		$('.dc-navbar-custom').removeClass('dc-navbar-fixed');
+	}
+});
