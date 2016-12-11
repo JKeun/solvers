f0a14a841f63b1b38a409f7faa726a7488444836
JKeun <jkeun1224@gmail.com>
2016-12-11 12:42:05 +0900

    merge about to index
    rm about page & add it to index page
    change navbar icons

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var indexRouter = require('./routes/index');
-var aboutRouter = require('./routes/about');
+// var aboutRouter = require('./routes/about');
 var projectsRouter = require('./routes/projects');
 
 var pug = require('pug');
@@ -22,7 +22,7 @@ app.set("port", process.env.PORT || 3030);
 
 // use router modules
 app.use("/", indexRouter);
-app.use("/", aboutRouter);
+// app.use("/", aboutRouter);
 app.use("/", projectsRouter);
 
 
